6b66fd6cb107b46d62eff564f0b1b6599cb870dd
seducerooo <94935126+seducerooo@users.noreply.github.com>
2023-06-13 12:08:48 +0330

    checkage middleware created then added to kernel && breeze package installed successfully

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\demo\DemoController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,10 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(DemoController::class)->group(function () {
-    Route::get('/about','About')->name('about.page');
-    Route::get('/contact','Contact')->name('contact.page');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
